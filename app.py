e636534d7b2638ea95923f2af074d3d5068040d4
rajaben-ali <54941748+rajaben-ali@users.noreply.github.com>
2021-06-04 18:53:27 +0200

    add: requirement file with plotly and dashboard with sidebar with the three vars parameters

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import plotly.express as px
 
 # This line needs to be the first one to be called in the script
 # Use the full page instead of a narrow central column
@@ -16,15 +17,24 @@ with st.beta_expander('Project description'):
   """)
 
 # Load the csv files
-columns_to_be_removed = ['Lat', 'Long']
-df_confirmed = pd.read_csv("./data/time_series_covid19_confirmed_global.csv").drop(columns_to_be_removed, axis = 'columns')
-df_deaths = pd.read_csv("./data/time_series_covid19_deaths_global.csv").drop(columns_to_be_removed, axis = 'columns')
-df_recovered = pd.read_csv("./data/time_series_covid19_recovered_global.csv").drop(columns_to_be_removed, axis = 'columns')
+@st.cache
+def load_data():
+  columns_to_be_removed = ['Lat', 'Long']
+  confirmed = pd.read_csv("./data/time_series_covid19_confirmed_global.csv").drop(columns_to_be_removed, axis = 'columns')
+  deaths = pd.read_csv("./data/time_series_covid19_deaths_global.csv").drop(columns_to_be_removed, axis = 'columns')
+  recovered = pd.read_csv("./data/time_series_covid19_recovered_global.csv").drop(columns_to_be_removed, axis = 'columns')
+  return confirmed, deaths, recovered
+
+df_confirmed, df_deaths, df_recovered = load_data()
 
 # Formatting of data
-df_confirmed.rename({'Province/State': 'province', 'Country/Region': 'country', 'Date': 'date'}, axis=1, inplace=True)
-df_deaths.rename({'Province/State': 'province', 'Country/Region': 'country', 'Date': 'date'}, axis=1, inplace=True)
-df_recovered.rename({'Province/State': 'province', 'Country/Region': 'country', 'Date': 'date'}, axis=1, inplace=True)
+@st.cache
+def format_data():
+  df_confirmed.rename({'Province/State': 'province', 'Country/Region': 'country', 'Date': 'date'}, axis=1, inplace=True)
+  df_deaths.rename({'Province/State': 'province', 'Country/Region': 'country', 'Date': 'date'}, axis=1, inplace=True)
+  df_recovered.rename({'Province/State': 'province', 'Country/Region': 'country', 'Date': 'date'}, axis=1, inplace=True)
+
+format_data()
 
 # Raw data - with streamlit cache
 @st.cache
@@ -58,19 +68,54 @@ st.sidebar.write("""
 # Control data to display here
 """)
 
-if st.sidebar.checkbox('Show data for reported cases'):
+if st.sidebar.checkbox('Reported cases'):
   # Content in sidebar
   st.sidebar.write("""
   ### Reported number of covid cases for one specific country
   """)
-  option = st.sidebar.selectbox(
-      'Which country do you want to dislay?',
+  option_confirmed = st.sidebar.selectbox(
+      'Cases - Which country do you want to dislay?',
       get_confirmed_melted(df_confirmed)["country"].unique())
   # Content in main interface
   df_confirmed_melted = get_confirmed_melted(df_confirmed)
-  chart_death = alt.Chart(df_confirmed_melted[df_confirmed_melted["country"] == option]).mark_line().encode(
-    x=alt.X('date:T'),
-    y=alt.Y('confirmed-count:Q'),
-    color=alt.Color("country:N")
-  ).properties(title="Confirmed number of cases for "+option)
-  st.altair_chart(chart_death, use_container_width=True)
+
+  # plot
+  st.write('### Reported number of covid cases in '+option_confirmed)
+  fig = px.line(df_confirmed_melted[df_confirmed_melted["country"] == option_confirmed], x="date", y="confirmed-count", hover_name="confirmed-count",
+          line_shape="spline", render_mode="svg")
+  st.plotly_chart(fig)
+
+if st.sidebar.checkbox('Death cases'):
+  # Content in sidebar
+  st.sidebar.write("""
+  ### Death number from covid cases for one specific country
+  """)
+  option_death = st.sidebar.selectbox(
+      'Death - Which country do you want to dislay?',
+      get_deaths_melted(df_deaths)["country"].unique())
+  # Content in main interface
+  df_death_melted = get_deaths_melted(df_deaths)
+
+  # plot
+  st.write('### Death number of people from covid in '+option_death)
+  fig_death = px.line(df_death_melted[df_death_melted["country"] == option_death], x="date", y="death-count", hover_name="death-count",
+          line_shape="spline", render_mode="svg")#, animation_frame="date")
+  st.plotly_chart(fig_death)
+
+
+if st.sidebar.checkbox('Recovered cases'):
+  # Content in sidebar
+  st.sidebar.write("""
+  ### Recovered number from covid cases for one specific country
+  """)
+  option_recov = st.sidebar.selectbox(
+      'Recovered - Which country do you want to dislay?',
+      get_recovered_melted(df_recovered)["country"].unique())
+  # Content in main interface
+  df_recovered_melted = get_recovered_melted(df_recovered)
+
+  # plot
+  st.write('### Recovered number of people from covid in '+option_recov)
+  fig_conf = px.line(df_recovered_melted[df_recovered_melted["country"] == option_recov], x="date", y="recovered-count", hover_name="recovered-count",
+          line_shape="spline", render_mode="svg")#, animation_frame="date")
+  st.plotly_chart(fig_conf)
